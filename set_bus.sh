90faa7bc04cfe321145577c7daddcb649962b367
root <root@arm.localdomain>
2016-11-30 02:55:02 +0000

    configure the pin

--- set_bus.sh
+++ set_bus.sh
@@ -1,70 +1,47 @@
+# Setting the Bus for Pins
+
 #echo cape-bone-iio > /sys/devices/bone_capemgr.*/slots
+# --> no need to echo this since the kernel version is different
 
-echo 7 > /sys/class/gpio/export
-echo out > /sys/class/gpio/gpio7/direction
-
-echo 110 > /sys/class/gpio/export
-echo out > /sys/class/gpio/gpio110/direction
-
-echo 111 > /sys/class/gpio/export
-echo out > /sys/class/gpio/gpio111/direction
-
-echo 112 > /sys/class/gpio/export
-echo out > /sys/class/gpio/gpio112/direction
-
-echo 113 > /sys/class/gpio/export
-echo out > /sys/class/gpio/gpio113/direction
-
-echo 73 > /sys/class/gpio/export
-echo out > /sys/class/gpio/gpio73/direction
-
-echo 71 > /sys/class/gpio/export
-echo out > /sys/class/gpio/gpio71/direction
-
-echo 30 > /sys/class/gpio/export
-echo out > /sys/class/gpio/gpio30/direction
-
-echo 60 > /sys/class/gpio/export
-echo out > /sys/class/gpio/gpio60/direction
-
-echo 31 > /sys/class/gpio/export
-echo out > /sys/class/gpio/gpio31/direction
-
-echo 50 > /sys/class/gpio/export
-echo in > /sys/class/gpio/gpio50/direction
-
-echo 51 > /sys/class/gpio/export
-echo in > /sys/class/gpio/gpio51/direction
-
-echo 38 > /sys/class/gpio/export
-echo out > /sys/class/gpio/gpio38/direction
-
-echo 39 > /sys/class/gpio/export
-echo out > /sys/class/gpio/gpio39/direction
-
-echo 34 > /sys/class/gpio/export
-echo out > /sys/class/gpio/gpio34/direction
-
-echo 35 > /sys/class/gpio/export
-echo out > /sys/class/gpio/gpio35/direction
-
-echo 4 > /sys/class/gpio/export
-echo out > /sys/class/gpio/gpio4/direction
-
-echo 5 > /sys/class/gpio/export
-echo out > /sys/class/gpio/gpio5/direction
-
+## Pin for Valves ##
+# CS1
 echo 51 > /sys/class/gpio/export
 echo out > /sys/class/gpio/gpio51/direction
 
-echo 12 > /sys/class/gpio/export
-echo out > /sys/class/gpio/gpio12/direction
+# CS2
+echo 7 > /sys/class/gpio/export
+echo out > /sys/class/gpio/gpio7/direction
 
-echo 13 > /sys/class/gpio/export
-echo out > /sys/class/gpio/gpio13/direction
-
+# Other
 echo 2 > /sys/class/gpio/export
 echo out > /sys/class/gpio/gpio2/direction
 
+# MOSI
+echo 112 > /sys/class/gpio/export
+echo out > /sys/class/gpio/gpio112/direction
+
+# SCLK
 echo 3 > /sys/class/gpio/export
 echo out > /sys/class/gpio/gpio3/direction
+
+
+## Pin for Sensors ##
+# CS
+echo 31 > /sys/class/gpio/export
+echo out > /sys/class/gpio/gpio31/direction
+
+# CLK
+echo 60 > /sys/class/gpio/export
+echo out > /sys/class/gpio/gpio60/direction
+
+# DIN
+echo 30 > /sys/class/gpio/export
+echo out > /sys/class/gpio/gpio30/direction
+
+# DOUT1
+echo 50 > /sys/class/gpio/export
+echo in > /sys/class/gpio/gpio50/direction
+
+# DOUT2
+echo 51 > /sys/class/gpio/export
+echo in > /sys/class/gpio/gpio51/direction
